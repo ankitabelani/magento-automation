25645d0c05ab5327926415a3d89e0d9fc07584c5
Ankita Belani <ankitabelani@gmail.com>
2025-03-04 15:42:45 +0530

    Added Login Page

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,33 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By signInLink = By.linkText("Sign In");
+    By emailField = By.id("email");
+    By passwordField = By.id("pass");
+    By loginButton = By.id("send2");
+
+    public void clickSignIn() {
+        driver.findElement(signInLink).click();
+    }
+
+    public void enterEmail(String email) {
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
 }
